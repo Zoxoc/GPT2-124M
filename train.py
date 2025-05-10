c4fe478215682ec9867064170d3c9a9ce629e089
Madhav Bhaskar <madhavbhaskar2005@gmail.com>
2025-05-11 03:28:36 +0530

    Implemented and verified input-target batching for training
    - Learned how to split a long sequence of tokens into (B, T) shape using view()
    - Created input and target tensors with correct next-token alignment in learning.ipynb
    - Applied and tested the same logic in the main file to confirm it works as expected

--- train.py
+++ train.py
@@ -192,46 +192,23 @@ class GPT(nn.Module):
 num_return_sequences = 5
 max_length = 30
 
-model = GPT.from_pretrained('gpt2')
-model.eval()
-#im using mps because i have a macbook, if you have a nvidia gpu use cuda
-model.to('mps')
+device = 'cpu' #im using mps because i have a macbook, if you have a nvidia gpu use cuda
 
-#prefix tokens
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long) #(8,)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) #(5, 8)
-x = tokens.to('mps')
+with open('tiny_shakespeare.txt', 'r') as f:
+    text = f.read()
+text = text[:1000]
+tokens = enc.encode(text)
+B, T = 4, 32
+buf = torch.tensor(tokens[:B*T + 1])
+x = buf[:-1].view(B, T)
+y = buf[1:].view(B, T)
 
-# generate! right now x is (B, T) where B = 5, T = 8
-# set the seed to 42
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-while x.size(1) < max_length:
-    # forward the model to get the logits
-    with torch.no_grad():
-        logits = model(x) # (B, T, vocab_size)
-        # take the logits at the last position
-        logits = logits[:, -1, :] # (B, vocab_size)
-        # get the probabilities
-        probs = F.softmax(logits, dim=-1)
-        # do top-k sampling of 50 (huggingface pipeline default)
-        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-        # select a token from the top-k probabilities
-        # note: multinomial does not demand the input to sum to 1
-        ix = torch.multinomial(topk_probs, 1) # (B, 1)
-        # gather the corresponding indices
-        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
-        # append to the sequence
-        x = torch.cat((x, xcol), dim=1)
+#torch randomly initialises the weights
+model = GPT(GPTConfig())
+model.to(device)
+logits = model(x)
 
-# print the generated text
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
-
-#for this specific output you can check the commit description
+print(logits.shape)
+import sys; sys.exit(0)
