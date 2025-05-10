cfe21eda38950a10b6e1cbbc507952841a2e1fb9
Madhav Bhaskar <madhavbhaskar2005@gmail.com>
2025-05-11 00:01:08 +0530

    added from_pretrained to load Hugging Face GPT-2 weights
    - Supports loading weights from gpt2, gpt2-medium, large, and xl
    - Creates a matching GPTConfig and model structure
    - Transposes select weights to match PyTorch Linear format
    - Skips buffer-only keys like attn.bias and masked_bias
    - Ensures 1-to-1 parameter shape alignment between HF and custom model

--- train.py
+++ train.py
@@ -13,13 +13,13 @@ from torch.nn import functional as F
 @dataclass
 class GPTConfig:
     block_size: int = 1024  # max sequence length
-    vocab_size: int = 50257
+    vocab_size: int = 50257  # 50,000 BPE Tokens + 256 bytes tokens + 1 <|endoftext|> token
     n_layer: int = 12
     n_head: int = 12
     n_embd: int = 768
 
 
-class CausalSelfAttention(nn.Module):
+class CasualSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
@@ -45,14 +45,10 @@ class CausalSelfAttention(nn.Module):
         q, k, v = qkv.split(self.n_embd, dim=2)
 
         # did the transpose so that PyTorch treats B, n_head as batches and trains them parallely
-        k = k.view(B, T, self.n_head, C //
-                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C //
-                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C //
-                   self.n_head).transpose(1, 2)  # (B, nh, T, hs)
-        y = F.scaled_dot_product_attention(
-            q, k, v, is_causal=True)  # flash attention
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)  # flash attention
 
         # re-assemble all head outputs side by side
         y = y.transpose(1, 2).contiguous().view(B, T, C)
@@ -107,3 +103,67 @@ class GPT(nn.Module):
             ln_f=nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+
+        # always 50257 for GPT model checkpoints
+        config_args['vocab_size'] = 50257
+
+        # always 1024 for GPT model checkpoints
+        config_args['block_size'] = 1024
+
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        # This creates a fresh model that matches GPT-2’s size — but it’s randomly initialized.
+        model = GPT(config)
+
+        sd = model.state_dict()  # sd --> state_dict()
+        sd_keys = sd.keys()
+
+        # Some keys are buffers, like attention masks — we don’t need them when copying weights.
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
+
+        # initialise a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)  # hf --> hugging face
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]  # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]  # same, just the mask (buffer)
+
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight','mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(
+            sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                
+                #Tells PyTorch "don't track gradients" while doing this copy - we’re not training here, just copying.
+                with torch.no_grad(): 
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
