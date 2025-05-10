dcb6824d8ffc25930823b4db33e72dabe766a19d
Madhav Bhaskar <madhavbhaskar2005@gmail.com>
2025-05-10 18:47:09 +0530

    Added Tranformer block and MLP
    - Added MLP class with linear -> GELU -> linear structure
    - Added Block class with LayerNorm, causal self-attention, and residual connections
    - These form the core of a transformer layer in the GPT architecture

--- train.py
+++ train.py
@@ -19,6 +19,38 @@ class GPTConfig:
     n_embd: int = 384
 
 
+class MLP(nn.Module):
+    """MLP or Feed-Forward Network"""
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    """Defining what a transformer block is"""
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
+
+
 class GPT(nn.Module):
 
     def __init__(self, config):
